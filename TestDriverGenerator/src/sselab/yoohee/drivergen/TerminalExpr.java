ab44d9d06d217810f52e7015edafb3f6f2d377ff
yoohee <jungyh0218@gmail.com>
2014-10-02 15:27:55 +0900

    added a constructor which can make a binary assign expression into 'equation' expression.

--- TestDriverGenerator/src/sselab/yoohee/drivergen/TerminalExpr.java
+++ TestDriverGenerator/src/sselab/yoohee/drivergen/TerminalExpr.java
@@ -3,7 +3,8 @@ package sselab.yoohee.drivergen;
 import sselab.cadd.cfg.expression.Expression;
 
 /**
- * @version 2014-09-19
+ * A single binary expression
+ * @version 2014-10-02
  * @author yoohee
  *
  */
@@ -12,7 +13,9 @@ public class TerminalExpr extends Expr{
 	public TerminalExpr(Expression _expression){
 		super.code = _expression.getRawString();
 	}
-	
+	public TerminalExpr(Expression operand1, String op, Expression operand2){
+		super.code = operand1.getRawString()+" == "+operand2.getRawString();
+	}
 	public String getCode(){
 		return code;
 	}
